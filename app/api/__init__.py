716775750b1785d1c0ab9f3392749a22741dcd91
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-09-13 01:28:49 +0530

    api

---
+++ app/api/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.api.latest_data_router import latest_router
+from app.api.history_data_router import history_router
+
+routers = APIRouter()
+
+routers.include_router(router=latest_router, prefix="/latest")
+routers.include_router(router=history_router, prefix="/history")
