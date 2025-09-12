e5dde58aa6fd99f2e12b790aa9c521489457ce24
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-09-13 01:27:33 +0530

    latest_data_service

---
+++ app/services/latest_data_service.py
@@ -0,0 +1,10 @@
+from app.schemas.responses import LatestData
+from app.repository.stock_data_repository import StockDataRepository
+
+
+class LatestDataService:
+    def __init__(self):
+        pass
+
+    def get_latest_data(self, symbol: str) -> LatestData:
+        return StockDataRepository.get_latest_by_symbol(symbol=symbol)
