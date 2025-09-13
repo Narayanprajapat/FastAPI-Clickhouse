3581dc66a6077407f14d15a0e36c51dead284360
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-09-13 16:55:38 +0530

    ingestion

---
+++ ingestion_services/clickhouse_connection.py
@@ -0,0 +1,42 @@
+from clickhouse_pool import ChPool
+
+clickhouse_connections_min = 1
+clickhouse_connections_max = 5
+
+clickhouse_host = "localhost"
+clickhouse_username = "default"
+clickhouse_password = 1234
+
+
+class ClickhousePool:
+    def __init__(self):
+        self.pool = None
+        self.client = None
+        self.connect()
+
+    def connect(self):
+        try:
+            self.pool = ChPool(
+                host=clickhouse_host,
+                user=clickhouse_username,
+                password=clickhouse_password,
+                connections_min=clickhouse_connections_min,
+                connections_max=clickhouse_connections_max,
+            )
+            self.client = self.pool.get_client()
+            print("Clickhouse successfully connected")
+
+        except Exception as e:
+            print(f"Getting exception while trying to connect with clickhouse-{e}")
+            raise Exception(
+                f"Getting exception while trying to connect with clickhouse-{e}"
+            )
+
+    def close_connection(self):
+        self.pool.cleanup()
+        self.pool = None
+        self.client = None
+        print("Clickhouse connection close")
+
+
+clickhouse_pool = ClickhousePool()
---
+++ ingestion_services/data_ingestion.py
@@ -0,0 +1,57 @@
+import time
+import yfinance as yf
+from datetime import datetime
+from clickhouse_connection import clickhouse_pool
+
+
+tickers = [
+    "RELIANCE.NS",  # Reliance Industries
+    "TCS.NS",  # Tata Consultancy Services
+    "HDFCBANK.NS",  # HDFC Bank
+    "INFY.NS",  # Infosys
+    "SBIN.NS",  # State Bank of India
+    "HINDUNILVR.NS",  # Hindustan Unilever
+    "BAJAJFINSV.NS",  # Bajaj Finserv
+    "ONGC.NS",  # Oil and Natural Gas Corp
+    "NTPC.NS",  # NTPC Limited
+    "TITAN.NS",  # Titan Company
+]
+
+
+def fetch_prices():
+    data = []
+    for t in tickers:
+        stock = yf.Ticker(t)
+        info = stock.fast_info
+        now = datetime.now()
+        print(info)
+        json_output = {
+            "event_time": datetime.now(),
+            "date": now.strftime("%Y-%m-%d"),
+            "symbol": t.replace(".NS", ""),
+            "open": float(info["open"]),
+            "high": float(info["day_high"]),
+            "low": float(info["day_low"]),
+            "close": float(info["last_price"]),
+            "volume": int(info["last_volume"]),
+        }
+        data.append(json_output)
+
+    print("data fetched succesfully")
+    with clickhouse_pool.client as client:
+        insert_query = "INSERT INTO market.ticks (event_time, date, symbol, open, high, low, close, volume) VALUES"
+        client.execute(insert_query, data)
+        # client.insert("market.ticks", data)
+
+    print("data inserted sucessfully into clickhouse")
+
+
+def main() -> None:
+    print("start")
+    while True:
+        fetch_prices()
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
