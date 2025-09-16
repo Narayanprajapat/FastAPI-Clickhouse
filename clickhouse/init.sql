f9b58cd0e9502dfcca78dc586a7bcc4913bd3232
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-09-16 21:24:41 +0530

    init.sql

--- clickhouse/init.sql
+++ clickhouse/init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS market;
 
-CREATE TABLE IF NOT EXISTS market.ticks(
+CREATE TABLE IF NOT EXISTS market.ticks
+(
     event_time   DateTime,
     date         Date,
     symbol       String,
@@ -8,7 +9,9 @@ CREATE TABLE IF NOT EXISTS market.ticks(
     high         Float64,
     low          Float64,
     close        Float64,
-    volume       UInt64
+    volume       UInt64,
+    prev_close_price Float64,
+    created_at   DateTime DEFAULT now()
 )
 ENGINE = MergeTree()
 PARTITION BY toYYYYMM(date)
